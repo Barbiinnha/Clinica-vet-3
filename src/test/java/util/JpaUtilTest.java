24adf72bafa317196d721d1f9a88166673c22845
Barbara Moroni <116181111+Barbiinnha@users.noreply.github.com>
2022-11-16 11:37:40 -0300

    Add files via upload

---
+++ src/test/java/util/JpaUtilTest.java
@@ -0,0 +1,43 @@
+package util;
+
+import javax.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author temporario
+ */
+public class JpaUtilTest {
+    
+    public JpaUtilTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+   
+    @Test
+    public void testEntityManager() {
+        EntityManager em = JpaUtil.getEntityManager(); 
+        assertNotNull(em);
+    }
+    
+}
